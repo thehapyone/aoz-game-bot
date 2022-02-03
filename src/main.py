1a40eace8a9dfe8114ca7ed9054e764781f6d695
Ayo Ayibiowu <charlesayibiowu@hotmail.com>
2022-02-03 22:47:34 +0100

    Refactor -  - Migrated all radar functionality in the zombie code to radar code.

--- src/main.py
+++ src/main.py
@@ -11,12 +11,16 @@ if __name__ == '__main__':
     launcher = GameLauncher(mouse, keyboard)
     launcher.start_game()
 
+    #radar = Radar(launcher)
+    #print(radar.get_current_level())
+
     # zombie
     time.sleep(5)
     zombie = Zombies(launcher)
     zombie.initialize_zombie()
     print('------------------------.-----------------')
-    zombie.kill_zombies(24, fleet=3)
+    zombie.kill_zombies(24, fleet=2)
+
     '''
     # farming
     time.sleep(5)
--- src/radar.py
+++ src/radar.py
@@ -5,11 +5,12 @@ from datetime import timedelta
 import cv2
 import numpy as np
 
-from src.constants import OUTSIDE_VIEW, BOTTOM_IMAGE, TOP_IMAGE, RIGHT_IMAGE
+from src.constants import OUTSIDE_VIEW, BOTTOM_IMAGE, TOP_IMAGE, RIGHT_IMAGE, \
+    LEFT_IMAGE
 from src.exceptions import RadarException
 from src.game_launcher import GameLauncher
 from src.helper import Coordinates, GameHelper, retry
-from src.ocr import get_text_from_image
+from src.ocr import get_text_from_image, ocr_from_contour
 
 
 class Radar:
@@ -28,7 +29,32 @@ class Radar:
             cls._instance._activated = False
             cls._instance._radar_coordinates = None
             cls._instance._go_button = None
+            cls._decrease_btn_cords = None
+            cls._increase_btn_cords = None
+            cls._set_out_btn_cords = None
         return cls._instance
+
+    @property
+    def decrease_btn_cords(self) -> Coordinates:
+        """
+        Returns the decrease btn coordinates.
+
+        :return: Coordinates of the button
+        """
+        if not self._decrease_btn_cords:
+            self.get_increase_decrease_btn_positions()
+        return self._decrease_btn_cords
+
+    @property
+    def increase_btn_cords(self) -> Coordinates:
+        """
+        Returns the increase btn coordinates.
+
+        :return: Coordinates of the button
+        """
+        if not self._increase_btn_cords:
+            self.get_increase_decrease_btn_positions()
+        return self._increase_btn_cords
 
     @property
     def go_button(self) -> Coordinates:
@@ -208,3 +234,164 @@ class Radar:
                     "Could not extract set out time."
                     f"Invalid time detected - {result}")
         return int(delta.total_seconds())
+
+    def get_increase_decrease_btn_positions(self):
+        """
+        Fetches the positions for the increase and decrease buttons
+        :return:
+        """
+        button_section, cords_relative = self.launcher. \
+            get_screen_section(10, BOTTOM_IMAGE)
+        # get the decrease button
+        decrease_section, decrease_relative = self.launcher. \
+            get_screen_section(30, LEFT_IMAGE,
+                               button_section, cords_relative)
+        decrease_cords = self.launcher.find_target(
+            decrease_section,
+            self.launcher.target_templates('zombie-decrease'))
+        if not decrease_cords:
+            raise RadarException("Decrease button not found")
+        self._decrease_btn_cords = GameHelper.get_relative_coordinates(
+            decrease_relative,
+            decrease_cords)
+        # get the increase button
+        increase_section, increase_relative = self.launcher. \
+            get_screen_section(40, RIGHT_IMAGE,
+                               button_section, cords_relative)
+        increase_cords = self.launcher.find_target(
+            increase_section,
+            self.launcher.target_templates('zombie-increase'))
+        if not increase_cords:
+            raise RadarException("Increase button not found")
+        self._increase_btn_cords = GameHelper.get_relative_coordinates(
+            increase_relative,
+            increase_cords)
+
+    def adjust_level(self, level_type: str, increase_count: int):
+        """
+        Adjust the current level to match the expected level
+        :return:
+        """
+        self.launcher.log_message(f'Adjusting level - {level_type}, '
+                                  f'{increase_count}')
+        if level_type == 'INCREASE':
+            self.launcher.mouse.set_position(
+                self.increase_btn_cords.start_x,
+                self.increase_btn_cords.start_y)
+            center = GameHelper.get_center(self.increase_btn_cords)
+            self.launcher.mouse.move(*center)
+        elif level_type == 'DECREASE':
+            self.launcher.mouse.set_position(
+                self.decrease_btn_cords.start_x,
+                self.decrease_btn_cords.start_y)
+            center = GameHelper.get_center(self.decrease_btn_cords)
+            self.launcher.mouse.move(*center)
+        else:
+            raise RadarException('Level type not supported')
+        time.sleep(1)
+        for _ in range(increase_count):
+            self.launcher.mouse.click()
+            time.sleep(0.5)
+
+    def set_level(self, level: int, max_level: int):
+        """
+        Set the level. If level is greater than max level.
+        Set to max level - 1
+
+        :param max_level: The max level
+        :param level: The expected level.
+        :returns: None
+        """
+        new_level = max_level if level > max_level else level
+        # enforces new level is a positive real number
+        new_level = new_level if new_level > 0 else 1
+        current_level = self._get_current_level()
+        increase_count = abs(new_level - current_level)
+        self.launcher.log_message(f'Adjusting level - {new_level}')
+        if new_level > current_level:
+            level_type = 'INCREASE'
+            self.adjust_level(level_type, increase_count)
+        elif new_level < current_level:
+            level_type = 'DECREASE'
+            self.adjust_level(level_type, increase_count)
+
+    def _get_current_level(self):
+        """
+        Fetches the current level.
+        :return:
+        """
+        bottom_section, _ = self.launcher. \
+            get_screen_section(13, BOTTOM_IMAGE)
+
+        t_h, t_w, _ = bottom_section.shape
+
+        level_section = bottom_section[
+                        0:t_h - int(0.52 * t_h),
+                        int(0.35 * t_w): t_w - int(0.35 * t_w)
+                        ]
+
+        white_min = (180, 180, 180)
+        white_max = (255, 255, 255)
+        image_processed = cv2.inRange(level_section, white_min, white_max)
+        custom_config = r'-c tessedit_char_whitelist=0123456789 ' \
+                        r'--oem 3 --psm 6'
+        custom_config2 = r'-c tessedit_char_whitelist=0123456789 ' \
+                         r'--oem 3 --psm 10'
+        zombie_level_val = get_text_from_image(image_processed,
+                                               custom_config)
+        zombie_level_val = zombie_level_val if zombie_level_val else \
+            ocr_from_contour(image_processed, custom_config2)
+        if zombie_level_val:
+            self.launcher.log_message(
+                f"Current level - {zombie_level_val}")
+            return int(zombie_level_val.strip())
+
+        raise RadarException("Current level can not be extracted")
+
+    @retry(exception=RadarException,
+           message="No set-out button found",
+           attempts=2)
+    def find_set_out(self) -> tuple[Coordinates, int]:
+        """
+        Finds out the set out button and also extract the time of trip.
+
+        :return: The set out button coordinates and the set out time.
+        """
+        if not self._set_out_btn_cords:
+            bottom_section, cords_relative = self.launcher. \
+                get_screen_section(13, BOTTOM_IMAGE)
+
+            bottom_section, cords_relative = self.launcher. \
+                get_screen_section(45, RIGHT_IMAGE,
+                                   bottom_section, cords_relative)
+            cords = self.launcher.find_target(
+                bottom_section,
+                self.launcher.target_templates('setout'))
+            if not cords:
+                raise RadarException("No set-out button found")
+            cords_relative = GameHelper. \
+                get_relative_coordinates(
+                cords_relative, cords)
+            self._set_out_btn_cords = cords_relative
+        time_section = self.launcher.get_screenshot()[
+                       self._set_out_btn_cords.start_y - 35:
+                       self._set_out_btn_cords.start_y,
+                       self._set_out_btn_cords.start_x:
+                       self._set_out_btn_cords.end_x - 20]
+        set_time = self.get_set_out_time(time_section)
+        return self._set_out_btn_cords, set_time
+
+    def send_fleet(self) -> int:
+        """
+        Sends out a troop fleet out to the target and also returns their set
+        out time.
+
+        :return: The troop set out time.
+        """
+        position, time_out = self.find_set_out()
+        if time_out:
+            self.launcher.mouse.set_position(position.start_x,
+                                             position.start_y)
+            self.launcher.mouse.move(GameHelper.get_center(position))
+            self.launcher.mouse.click()
+        return time_out
